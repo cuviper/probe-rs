f5d329f94b74503e423d6ab5c64943de43bd8349
Josh Stone <jistone@redhat.com>
2014-07-08 15:39:17 -0700

    Publish libprobe documentation

---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['probe'] = {"items":[[0,"","probe","This crate provides static instrumentation macros."],[15,"probe!","","Define a static probe point."]],"paths":[]};
+initSearch(searchIndex);
