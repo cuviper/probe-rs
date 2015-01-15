918f7d521a2449fa458185b5b892fd3afa23609a
Josh Stone <jistone@redhat.com>
2015-01-14 23:29:12 -0800

    Add "loop" as a standalone example

---
+++ examples/loop.rs
@@ -0,0 +1,12 @@
+#[macro_use] #[no_link]
+extern crate probe;
+fn main() {
+    probe!(foo, begin);
+    let mut total = 0;
+    for i in 0..100 {
+        total += i;
+        probe!(foo, loop, i, total);
+    }
+    assert_eq!(total, 4950);
+    probe!(foo, end);
+}
