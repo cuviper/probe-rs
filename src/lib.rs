905cbebf6236523e1e58db96957dac40f4227277
Josh Stone <jistone@redhat.com>
2015-04-27 12:47:40 -0700

    drop unstable tag

--- src/lib.rs
+++ src/lib.rs
@@ -92,7 +92,6 @@
 
 #![crate_name = "probe"]
 #![crate_type = "dylib"]
-#![unstable]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/nightly/")]
