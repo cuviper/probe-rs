7e124c0d17c361a5cae2d24549b6d8ee2e016e80
Josh Stone <cuviper@gmail.com>
2020-05-20 13:38:56 -0700

    Ignore "Gap in build notes detected"

--- tests/readelf.rs
+++ tests/readelf.rs
@@ -16,7 +16,14 @@ fn check_notes() {
     let test_exe = env::current_exe().unwrap();
     let output = Command::new("readelf").arg("-n").arg(&test_exe).output().unwrap();
     assert!(output.status.success());
-    assert!(output.stderr.is_empty());
+
+    for error in String::from_utf8_lossy(&output.stderr).lines() {
+        if error.contains("Warning: Gap in build notes detected") {
+            continue;
+        }
+        panic!("{}", error);
+    }
+
     let count = String::from_utf8_lossy(&output.stdout).lines()
         .filter(|line| line.contains("NT_STAPSDT")).count();
     assert_eq!(count, 2);
