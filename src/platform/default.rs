60812dd7415fafe7c075e52832e88366ea11a50a
Josh Stone <cuviper@gmail.com>
2020-05-29 12:40:54 -0700

    Allow unlimited arguments in SystemTap probes

--- src/platform/default.rs
+++ src/platform/default.rs
@@ -11,5 +11,5 @@
 #[doc(hidden)]
 #[macro_export]
 macro_rules! platform_probe(
-    ($provider:ident, $name:ident, $($arg:expr),*) => ()
+    ($provider:ident, $name:ident, $($arg:expr,)*) => ()
 );
