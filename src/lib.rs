bc00d6cfb3495a387d1fac3cc7ed3b7175d4f9cb
Josh Stone <cuviper@gmail.com>
2022-01-14 14:58:08 -0800

    Remove outdated crate metadata

--- src/lib.rs
+++ src/lib.rs
@@ -88,13 +88,6 @@
 //! $2 = 1035
 //! ```
 
-#![crate_name = "probe"]
-#![crate_type = "rlib"]
-#![doc(
-    html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
-    html_favicon_url = "http://www.rust-lang.org/favicon.ico",
-    html_root_url = "http://doc.rust-lang.org/nightly/"
-)]
 #![no_std]
 
 mod platform;
