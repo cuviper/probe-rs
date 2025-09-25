90cabd16249d225318f4faf500dd58c5d3a70f65
Paolo Bonzini <pbonzini@redhat.com>
2025-09-25 14:46:34 +0200

    Fix clippy "needless_doctest_main" lint
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -14,16 +14,14 @@
 //!
 //! ```rust
 //! use probe::probe;
-//! fn main() {
-//!     probe!(foo, begin);
-//!     let mut total = 0;
-//!     for i in 0..100 {
-//!         total += i;
-//!         probe!(foo, loop, i, total);
-//!     }
-//!     assert_eq!(total, 4950);
-//!     probe!(foo, end);
+//! probe!(foo, begin);
+//! let mut total = 0;
+//! for i in 0..100 {
+//!     total += i;
+//!     probe!(foo, loop, i, total);
 //! }
+//! assert_eq!(total, 4950);
+//! probe!(foo, end);
 //! ```
 //!
 //! ## Using probes with SystemTap
