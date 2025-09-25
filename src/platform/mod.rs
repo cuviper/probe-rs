3b5a763093c05d0a65fa0e608f4ca98c3149adb0
Paolo Bonzini <pbonzini@redhat.com>
2025-09-25 12:15:49 +0200

    Pick the systemtap backend for docs.rs
    Ensure that Semaphore is documented properly.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,9 +1,9 @@
-#[cfg(any(target_os = "linux", target_os = "android"))]
+#[cfg(any(target_os = "linux", target_os = "android", docsrs))]
 mod systemtap;
-#[cfg(any(target_os = "linux", target_os = "android"))]
+#[cfg(any(target_os = "linux", target_os = "android", docsrs))]
 pub use systemtap::*;
 
-#[cfg(not(any(target_os = "linux", target_os = "android")))]
+#[cfg(not(any(target_os = "linux", target_os = "android", docsrs)))]
 mod default;
-#[cfg(not(any(target_os = "linux", target_os = "android")))]
+#[cfg(not(any(target_os = "linux", target_os = "android", docsrs)))]
 pub use default::*;
